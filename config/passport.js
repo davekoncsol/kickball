b7881e375d6fe07566189d0d80ce4dcd35db054c
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-08-23 11:07:56 -0700

    initial set up, linked to atlas db, linked to Oauth, next step, set up pages

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var Baller = require('../models/baller');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    Baller.findOne({ 'googleId': profile.id }, function(err, baller) {
+      if (err) return cb(err);
+      if (baller) {
+        return cb(null, baller);
+      } else {
+        // we have a new Baller via OAuth!
+        var newBaller = new Baller({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
+         
+        });
+        newBaller.save(function(err) {
+          if (err) return cb(err);
+          return cb(null, newBaller);
+        });
+      }
+    });
+  }
+));
+
+passport.serializeUser(function(baller, done){
+    done(null, baller.id);
+})
+
+passport.deserializeUser(function(id, done){
+    Baller.findById(id, function(err, baller){
+        done(err, baller);
+    })
+})
---
+++ models/baller.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var commentSchema = new mongoose.Schema({
+    text: String
+  }, {
+    timestamps: true
+  });
+
+var ballerSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    team: String,
+    avatar: String,
+    facts: [commentSchema],
+    googleId: String
+  }, {
+    timestamps: true
+  });
+
+  module.exports = mongoose.model('Baller', ballerSchema)
